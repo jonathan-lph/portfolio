b432b0f9f26890ada65366881a554139e766e63f
jonathan-lph <jonathan.lph@hotmail.com>
2022-04-05 17:06:28 +0800

    Tuned responsive paddings and layout

--- src/components/project/feature/index.js
+++ src/components/project/feature/index.js
@@ -4,23 +4,25 @@ export default function Features({ image_dir, features, images }) {
 
   return (
     <main className={styles.root}>
-      <div className={styles.gallery}>
-        {images.map(image =>
-          <img
-            src={`/image/${image_dir}/${image}.png`}
-            key={image}
-          />
-        )}
-      </div>
-      <div className={styles.features}>
-        {features.map(({ title, desc }) =>
-          <div className={styles.feature} key={title}>
-            <h2>{title}</h2>
-            <p>
-              {desc}
-            </p>
-          </div>  
-        )}
+      <div className={styles.inner}>
+        <div className={styles.gallery}>
+          {images.map(image =>
+            <img
+              src={`/image/${image_dir}/${image}.png`}
+              key={image}
+            />
+          )}
+        </div>
+        <div className={styles.features}>
+          {features.map(({ title, desc }) =>
+            <div className={styles.feature} key={title}>
+              <h2>{title}</h2>
+              <p>
+                {desc}
+              </p>
+            </div>  
+          )}
+        </div>
       </div>
     </main>
   )
