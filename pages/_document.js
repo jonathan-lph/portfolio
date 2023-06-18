401405f07a445c2b5dbf5a8366cdff38f25dfc98
jonathan-lph <jonathan.lph@hotmail.com>
2023-06-18 01:00:01 +0100

    Changed title font and related styles

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-        <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@200;300;400&family=Syne:wght@400;700;800&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@200;300;400&family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet"/>
       </Head>
       <body>
         <Main />
--- src/components/home/jumbotron/index.js
+++ src/components/home/jumbotron/index.js
@@ -1,19 +1,23 @@
-import styles from './styles.module.sass'
 import personal from "@util/personal.json";
+import styles from "./styles.module.sass";
 
 export default function Jumbotron() {
   return (
     <section className={styles.profile}>
       <div className={styles.jumbo}>
-        <img src="/image/profile.jpg" className={styles.jumboImg}/>
-        <div className={styles.jumboTitle}>
+        <div className={styles.inner}>
+          <div className={styles.imgWrapper}>
+            <img src="/image/profile.jpg" className={styles.jumboImg} />
+          </div>
+          <div className={styles.jumboTitle}>
             <h1>{personal.name}</h1>
             <h2>{personal.title}</h2>
+          </div>
         </div>
         <svg viewBox="0 0 203 34" fill="none" className={styles.arrowDown}>
-          <path d="M202 1L101.5 32.5L1 1"/>
+          <path d="M202 1L101.5 32.5L1 1" />
         </svg>
       </div>
     </section>
-  )
+  );
 }
