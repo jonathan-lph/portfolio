0834cc72aba7dddd6c5cd46e92e9079583f398d7
jonathan-lph <jonathan.lph@hotmail.com>
2023-07-12 00:56:49 +0100

    Lazy loaded images

--- src/app/components/projects/Project.tsx
+++ src/app/components/projects/Project.tsx
@@ -24,6 +24,7 @@ export default function Project({
   const [isExpanded, setIsExpanded] = useState(false)
   const [height, setHeight] = useState(0)
   const rootRef = useRef<HTMLLIElement | null>(null)
+  const imagesRef = useRef<HTMLDivElement | null>(null)
   const contentRef = useRef<HTMLDivElement | null>(null)
   const shouldRenderContent = useDelayUnmount(isExpanded, 600)
   const shouldRenderTooltip = useDelayUnmount(isExpanded, 400 + 600)
@@ -42,6 +43,22 @@ export default function Project({
       setHeight(contentRef.current?.getBoundingClientRect().height ?? 0)
     else setHeight(0)
   }, [shouldRenderContent])
+
+  useEffect(() => {
+    if (!rootRef.current) return
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting && imagesRef.current) {
+        imagesRef.current.childNodes.forEach(
+          (node) => {(node as HTMLImageElement).loading = 'eager'}
+        )
+      }
+    })
+    observer.observe(rootRef.current)
+    return () => {
+      if (!rootRef.current) return
+      observer.unobserve(rootRef.current)
+    }
+  }, [])
 
   return (
     <li ref={rootRef}>
@@ -128,9 +145,14 @@ export default function Project({
                 </ExternalButton>
               )}
             </div>
-            <div className={styles.previews}>
+            <div className={styles.previews} ref={imagesRef}>
               {previews.map((preview, idx) => (
-                <img src={preview} key={preview} alt={`Image ${idx} for project ${name}`} />
+                <img
+                  src={preview}
+                  key={preview}
+                  alt={`Image ${idx} for project ${name}`}
+                  loading="lazy"
+                />
               ))}
             </div>
             <button
