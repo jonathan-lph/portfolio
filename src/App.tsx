dbf1b6e7399d2aaeafc511bc343673ee2ff59d5d
Jonathan Lam <jonathan.lam98@gmail.com>
2026-08-18 15:54:24 +0100

    refactor: constrain Header content to max-width

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import Header from './sections/Header/Header'
+
 function App() {
-  return null
+  return <Header />
 }
 
 export default App
--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -1,16 +1,19 @@
 import { Hand } from 'lucide-react'
 import NavMenu from '../../components/NavMenu/NavMenu'
 import Divider from '../../components/Divider/Divider'
+import Container from '../../components/Container/Container'
 import styles from './Header.module.css'
 
 function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.banner}>
-        <Hand strokeWidth={1.5} className={styles.handIcon} />
-        <NavMenu />
+        <Container className={styles.bannerInner}>
+          <Hand strokeWidth={1.5} className={styles.handIcon} />
+          <NavMenu />
+        </Container>
       </div>
-      <div className={styles.intro}>
+      <Container className={styles.intro}>
         <h1 className={styles.name}>jonathan lam</h1>
         <p className={styles.description}>
           Hi there, I am Jonathan, a full-stack developer based in London, currently working in{' '}
@@ -20,7 +23,7 @@ function Header() {
           . I enjoy building technical solutions, writing proses, and documenting the small things
           in life.
         </p>
-      </div>
+      </Container>
       <Divider />
     </header>
   )
