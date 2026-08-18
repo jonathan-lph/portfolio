9da0a5a469bb5e6e045c852678845d2cf8855022
Jonathan Lam <jonathan.lam98@gmail.com>
2026-08-18 12:57:54 +0100

    feat: add BackToTop component

---
+++ src/components/BackToTop/BackToTop.tsx
@@ -0,0 +1,16 @@
+import { ArrowUp } from 'lucide-react'
+import styles from './BackToTop.module.css'
+
+function BackToTop() {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+
+  return (
+    <button type="button" className={styles.button} onClick={scrollToTop} aria-label="Back to top">
+      <ArrowUp strokeWidth={1.5} />
+    </button>
+  )
+}
+
+export default BackToTop
