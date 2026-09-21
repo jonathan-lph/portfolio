b1c6a0403d56d6e48c089020dc82ca73d5e311cc
Jonathan Lam <jonathan.lam98@gmail.com>
2026-09-21 23:38:18 +0100

    feat: add Contact section
    Mirror the About layout with columns reversed: the "contact" SectionLabel
    illustration on the left and the contact details (ContactList + ResumeButton)
    on the right, stacking on mobile.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from './sections/Header/Header'
 import About from './sections/About/About'
 import Projects from './sections/Projects/Projects'
+import Contact from './sections/Contact/Contact'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <About />
       <Projects />
+      <Contact />
     </>
   )
 }
---
+++ src/sections/Contact/Contact.tsx
@@ -0,0 +1,29 @@
+import BackToTop from '../../components/BackToTop/BackToTop'
+import ContactList from '../../components/ContactList/ContactList'
+import Container from '../../components/Container/Container'
+import Divider from '../../components/Divider/Divider'
+import ResumeButton from '../../components/ResumeButton/ResumeButton'
+import SectionLabel from '../../components/SectionLabel/SectionLabel'
+import styles from './Contact.module.css'
+
+function Contact() {
+  return (
+    <section id="contact" className={styles.contact}>
+      <Container className={styles.row}>
+        <div className={styles.illustration}>
+          <SectionLabel label="contact" />
+        </div>
+        <div className={styles.details}>
+          <ContactList />
+          <ResumeButton />
+        </div>
+      </Container>
+      <Container className={styles.end}>
+        <BackToTop />
+      </Container>
+      <Divider />
+    </section>
+  )
+}
+
+export default Contact
