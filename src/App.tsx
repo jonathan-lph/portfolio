c75bbaf1e004da859047630d9d4ef27dfc69a1a0
Jonathan Lam <jonathan.lam98@gmail.com>
2026-09-21 23:49:55 +0100

    feat: render Footer in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from './sections/Header/Header'
 import About from './sections/About/About'
 import Projects from './sections/Projects/Projects'
 import Contact from './sections/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </>
   )
 }
